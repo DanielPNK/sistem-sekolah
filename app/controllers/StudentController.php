6780c5d3de834d7fb0274f1ae973835d21998dbf
DanielPNK <danielpnkkdw@gmail.com>
2026-04-21 08:18:58 +0700

    feat: implement create page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -38,4 +38,11 @@ class StudentController extends Controller
     {
         $this->view('students.edit');
     }
+
+    public function store()
+    {
+        $studentModel = new Student();
+        $studentModel->insert($_POST);;
+    }
 }
+
